3124d26069482bf7ad476084587258056ac23e43
Danthie <114122786+Danthiee@users.noreply.github.com>
2023-11-26 09:19:02 -0600

    ok

---
+++ src/Hw2/2.2/make-multi-filter.js
@@ -0,0 +1,14 @@
+function MakeMultiFilter(array) {
+    var currentArray = array;
+    function arrayFilterer(filterCriteria, callback) {
+        if (typeof filterCriteria !== 'function') {
+            return currentArray;
+        }
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof callback === 'function') {
+            callback(currentArray);
+        }
+        return arrayFilterer;
+    }
+    return arrayFilterer;
+}
