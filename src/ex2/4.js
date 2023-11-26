44bea3e8d579bd226cbae1251878499793e764f6
Danthie <114122786+Danthiee@users.noreply.github.com>
2023-11-26 05:37:57 -0600

    ok

---
+++ src/ex2/4.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('my-link');
+    link.addEventListener('mouseover', function() {
+      var boldWords = document.querySelectorAll('strong');
+      boldWords.forEach(function(word) {
+        word.style.backgroundColor = 'yellow'; // Highlight bold words
+      });
+    });
+    link.addEventListener('mouseout', function() {
+      var boldWords = document.querySelectorAll('strong');
+      boldWords.forEach(function(word) {
+        word.style.backgroundColor = ''; // Remove highlight from bold words
+      });
+    });
+  });
---
+++ src/ex2/5.js
@@ -0,0 +1,5 @@
+window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    console.log('Window size:', width, 'x', height);
+  });
